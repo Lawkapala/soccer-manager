b4bdeb882f1751803b28ac5096aacb5fd5a4660e
Marc Ginovart Vega <mginov@gmail.com>
2015-11-08 23:46:05 +0100

    minor updates

--- docs/db_sx_fut7.sql
+++ docs/db_sx_fut7.sql
@@ -1,114 +1,50 @@
-/*
-SQLyog Ultimate v9.51 
-MySQL - 5.6.16 : Database - sx_fut7
-*********************************************************************
-*/
+# ************************************************************
+# Sequel Pro SQL dump
+# Versión 4499
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 5.6.26)
+# Base de datos: sx_fut7
+# Tiempo de Generación: 2015-11-08 22:42:19 +0000
+# ************************************************************
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`sx_fut7` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `sx_fut7`;
 
-/*Table structure for table `event` */
-
-DROP TABLE IF EXISTS `event`;
-
-CREATE TABLE `event` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(155) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `match` */
-
-DROP TABLE IF EXISTS `match`;
-
-CREATE TABLE `match` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `matchday_id` int(11) NOT NULL,
-  `home_team` int(11) NOT NULL,
-  `score_home_team` int(11) DEFAULT NULL,
-  `away_team` int(11) DEFAULT NULL,
-  `score_away_team` int(11) DEFAULT NULL,
-  `location` varchar(50) DEFAULT NULL,
-  `played` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `matchday_id` (`matchday_id`),
-  KEY `home_team` (`home_team`),
-  KEY `away_team` (`away_team`),
-  CONSTRAINT `match_ibfk_1` FOREIGN KEY (`matchday_id`) REFERENCES `matchday` (`id`),
-  CONSTRAINT `match_ibfk_2` FOREIGN KEY (`home_team`) REFERENCES `team` (`id`),
-  CONSTRAINT `match_ibfk_3` FOREIGN KEY (`away_team`) REFERENCES `team` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `match_event` */
-
-DROP TABLE IF EXISTS `match_event`;
-
-CREATE TABLE `match_event` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `match_id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  `event_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `matchday` */
-
-DROP TABLE IF EXISTS `matchday`;
-
-CREATE TABLE `matchday` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `player` */
+# Volcado de tabla player
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `player`;
 
 CREATE TABLE `player` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
+  `img_player` varchar(100) DEFAULT NULL,
   `position_id` int(11) NOT NULL,
+  `num_player` int(11) DEFAULT NULL,
   `team_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `position_id` (`position_id`),
   KEY `team_id` (`team_id`),
-  CONSTRAINT `player_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `postion` (`id`),
+  CONSTRAINT `player_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `player_position` (`id`),
   CONSTRAINT `player_ibfk_2` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Table structure for table `postion` */
 
-DROP TABLE IF EXISTS `player_position`;
 
-CREATE TABLE `player_position` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Table structure for table `team` */
-
-DROP TABLE IF EXISTS `team`;
-
-CREATE TABLE `team` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `shield_name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
